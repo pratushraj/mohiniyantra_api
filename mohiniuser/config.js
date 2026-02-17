3e132b58fb0ab85614d17598bed0ff513c4e3fad
pratushraj <pratushraj0@gmail.com>
2026-02-17 12:28:52 +0530

    Fixed

--- mohiniuser/config.js
+++ mohiniuser/config.js
@@ -152,9 +152,30 @@ const appGlobalConfig = {
 checkAuth();
 
 function logout() {
-    localStorage.setItem('cache', appGlobalConfig.userCache)
+    setCookie('cache', appGlobalConfig.userCache, 3650); // Store for 10 years (lifetime)
     localStorage.removeItem('loginInfo');
     window.location.href = 'login.html';
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
 }
 
 function closePopup() {
